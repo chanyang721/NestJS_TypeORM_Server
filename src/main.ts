e216c8e88b0fdc38b1e2134d90742342ede1b214
chanyang217 <chanyang721@gmail.com>
2022-01-07 23:57:23 +0900

    보안 관련 helmet csurf hash cors 설정

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import csurf from 'csurf';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+  app.use(helmet());
+  app.use(csurf());
   await app.listen(3000);
 
   if (module.hot) {
